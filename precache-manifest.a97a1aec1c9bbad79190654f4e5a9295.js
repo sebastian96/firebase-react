2029875206b411e889b04638f0273cb1ca970475
Sebastian Miranda <arqcompu4@gmail.com>
2020-08-02 11:43:47 -0500

    Updates

---
+++ precache-manifest.a97a1aec1c9bbad79190654f4e5a9295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e5aeb1508660931a7b11d9c9bede24",
+    "url": "/firebase-react/index.html"
+  },
+  {
+    "revision": "068e2973b6731886c41a",
+    "url": "/firebase-react/static/css/2.55367b3d.chunk.css"
+  },
+  {
+    "revision": "c7722235f4282c75b38d",
+    "url": "/firebase-react/static/css/main.87b10525.chunk.css"
+  },
+  {
+    "revision": "068e2973b6731886c41a",
+    "url": "/firebase-react/static/js/2.886e8016.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firebase-react/static/js/2.886e8016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7722235f4282c75b38d",
+    "url": "/firebase-react/static/js/main.3c6fa66a.chunk.js"
+  },
+  {
+    "revision": "e69f10d6139fc0bc40ff",
+    "url": "/firebase-react/static/js/runtime-main.40c2c6c0.js"
+  }
+]);
